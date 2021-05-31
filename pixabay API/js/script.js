99c0b7c91853b0568b2b35bbb85e63d1bdb6389b
Andrea <andrea3aprile99@gmail.com>
2021-05-31 21:08:11 +0200

    api

--- pixabay API/js/script.js
+++ pixabay API/js/script.js
@@ -8,7 +8,8 @@ new Vue({
       let link = 'https://pixabay.com/api/?key=21824357-188f94b37d0cfe213094797ce&q=yellow+flowers&image_type=photo'
       axios.get(link).then((resultate)=>{
           this.series = resultate.data.hits;
-          console.log(resultate.data);
+          console.log(resultate.data.hits);
+          
         });
       }
     },
